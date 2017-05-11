f7abd76eca38e2f87fd4fc8017ea6d48efa4fcb2
igorteste <igor.x.ndj@gmail.com>
2017-05-11 10:56:56 -0300

    criacao das classes persistencia e modelo

---
+++ src/java/modelo/Usuario.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author sala308b
+ */
+public class Usuario {
+    private String login;
+    private String nome;
+    private String senha;
+    private String perfil;
+    private String status;
+
+    public Usuario() {
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the perfil
+     */
+    public String getPerfil() {
+        return perfil;
+    }
+
+    /**
+     * @param perfil the perfil to set
+     */
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
---
+++ src/java/persistencia/UsuarioDAO.java
@@ -0,0 +1,85 @@
+package persistencia;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Usuario;
+
+public class UsuarioDAO {
+
+    private Connection conexao;
+
+    public UsuarioDAO() {
+        conexao = ConexaoFactory.getConnection();
+   
+ 
+    }
+
+    public boolean inserirCliente(Usuario u) {
+
+        boolean resultado = false;
+
+        String sql = "INSERT INTO usuarios (login, nome, senha, perfil, status)"  
+                                                   + " VALUES (?, ?, ?, ?, ?)";
+                 
+
+        try {
+            conexao.setAutoCommit(false);
+            
+            PreparedStatement stmt = conexao.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, u.getLogin());
+            stmt.setString(2, u.getNome());
+            stmt.setString(3, u.getSenha());
+            stmt.setString(4, u.getPerfil());
+            stmt.setString(5, u.getStatus());
+            
+            stmt.executeUpdate();
+
+            stmt.close();
+       
+                conexao.commit();
+                resultado = true;
+            
+            
+        } catch (SQLException ex) {
+            try {
+                conexao.rollback();
+            } catch (SQLException ex1) {
+               JOptionPane.showMessageDialog(null,"Erro ao reverter a operação - " + ex1.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+            JOptionPane.showMessageDialog(null,"Erro ao inserir o cliente - " + ex.getMessage());
+        } finally {
+
+            try {
+                conexao.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Erro ao fechar conexão - " + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+
+        return resultado;
+    }
+
+//    public boolean inserirEndereco(Cliente cliente) throws SQLException {
+//        não coloquei try-catch para se der erro, 
+//        o erro será tratado no método inserirCliente;
+//        boolean resultado = false;
+//        
+//        String sql = "INSERT INTO enderecos (rua, bairro, cod_cliente) VALUES (?,?,?)";
+//
+//        PreparedStatement stmt = conexao.prepareStatement(sql);
+//        stmt.setString(1, cliente.getEndereco().getRua());
+//        stmt.setString(2, cliente.getEndereco().getBairro());
+//        stmt.setInt(3, cliente.getCodigo());
+//
+//        stmt.executeUpdate();
+//        resultado = true;
+//        
+//        stmt.close();                
+//        return resultado;
+//    }
+
+}
